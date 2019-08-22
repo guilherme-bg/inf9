fc749279a3bcaeec24c5b3cec1674e79206fa3aa
guilherme-bg <guilhermebritogasparini@gmail.com>
2019-08-22 20:14:57 -0300

    pasta apagada

--- projeto_em_branco/app/blueprints/home/__init__.py
+++
@@ -1,4 +0,0 @@
-# app/blueprints/home/__init__.py
-from flask import Blueprint
-home = Blueprint ('home',__name__)
-from . import routes
--- projeto_em_branco/app/blueprints/home/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template
-from . import home
-
-@home.route('/')
-def index():
-    return "HOME"
